8b1edad3bf323ee62ef45a239aa3289e463d01f7
zhangxiaobo123 <2921633089@qq.com>
2020-11-18 13:48:38 +0800

    修改配置

--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,7 @@ module.exports = {
         exclude: /node_modules/
       },
       {
-        test: /\.(png|jpg|gif|svg)$/,
+        test: /\.(png|jpg|gif|svg|ttf)$/,
         loader: 'file-loader',
         options: {
           name: '[name].[ext]?[hash]'
